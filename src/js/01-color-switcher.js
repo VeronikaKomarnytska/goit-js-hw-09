d3236cbcaf110695d21d47d4bff72adaf5c47812
Veronika Komarnytska <113595751+VeronikaKomarnytska@users.noreply.github.com>
2023-03-19 13:43:55 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startButton.addEventListener('click', () => {
+  timer.start();
+});
+refs.stopButton.addEventListener('click', () => {
+  timer.stop();
+});
+
+const timer = {
+  start() {
+    refs.startButton.setAttribute('disabled', true);
+    refs.stopButton.removeAttribute('disabled');
+
+    const startTime = Date.now();
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      refs.body.style.backgroundColor = getRandomHexColor(currentTime);
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.timerId);
+    this.isActive = false;
+    refs.startButton.removeAttribute('disabled');
+    refs.stopButton.setAttribute('disabled', true);
+  },
+};
