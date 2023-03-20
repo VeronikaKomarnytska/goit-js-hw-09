6019b9b02d7330d41a6607c3c6b06063d3449089
VeronikaKomarnytska <113595751+VeronikaKomarnytska@users.noreply.github.com>
2023-03-20 18:10:53 +0000

    Deploying to gh-pages from @ VeronikaKomarnytska/goit-js-hw-09@aab1b292550a06311e8d7fa07fb8c4186d8c6b0a 🚀

---
+++ 03-promises.09dc4df9.js
@@ -0,0 +1,2 @@
+!function(){({form:document.querySelector(".form")}).form.addEventListener("submit",(function(){Math.random()}));new Promise((function(e,n){setTimeout((function(){e("Success")}),1e3)})).then().catch()}();
+//# sourceMappingURL=03-promises.09dc4df9.js.map
---
+++ 03-promises.a39bbf92.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".form")}).form.addEventListener("submit",(()=>{Math.random()}));new Promise(((e,t)=>{setTimeout((()=>{e("Success")}),1e3)})).then().catch();
+//# sourceMappingURL=03-promises.a39bbf92.js.map
